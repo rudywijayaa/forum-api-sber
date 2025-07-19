44fc5ae07105cea365a2462b3f67c6baf151b462
rudywijayaa <wijayarudy99@gmail.com>
2025-07-20 02:30:54 +0700

    add test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -38,7 +38,7 @@ describe('HTTP server', () => {
     expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
   });
 
-  describe('when GET /', () => {
+  describe('ketika GET /', () => {
     it('should return 200 and hello world', async () => {
       // Arrange
       const server = await createServer({});
